ed0134e99d8ebc47ac35aec4c3c7b84ea32a6d29
riddickam <82107655+riddickam@users.noreply.github.com>
2021-04-21 17:18:07 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,62 +1,62 @@
 <?php
 
-$BOT_TOKEN = "1787672753:AAHgbD1VYtlO49DpAPQIi2mgoO6hjLnhabs";
+// $BOT_TOKEN = "1787672753:AAHgbD1VYtlO49DpAPQIi2mgoO6hjLnhabs";
+// 
+// $parameters = array(
+//   "chat_id" => 1787672753,
+//   "text"    => "Ciao, utente"
+// );
+// 
+// send("sendMessage", $parameters);
+// function send($method, $data){
+//   global $BOT_TOKEN;
+//   $url = "https://api.telegram.org/bot$BOT_TOKEN/$method";
+// 
+//   if(!$curl = curl_init()){
+//     exit;
+//   }
+//   curl_setopt($curld, CURLOPT_POST, true);
+//   curl_setopt($curld, CURLOPT_POSTFIELDS, $data);
+//   curl_setopt($curld, CURLOPT_URL, $url);
+//   curl_setopt($curld, CURLOPT_RETURNTRANSFER, true);
+//   $output = curl_exec($curld);
+//   curl_close($curld);
+//   return $output;
+// }
 
-$parameters = array(
-  "chat_id" => 1787672753,
-  "text"    => "Ciao, utente"
-);
+$botToken = '1787672753:AAHgbD1VYtlO49DpAPQIi2mgoO6hjLnhabs';
+$website = 'https://api.telegram.org/bot'.$botToken;
 
-send("sendMessage", $parameters);
-function send($method, $data){
-  global $BOT_TOKEN;
-  $url = "https://api.telegram.org/bot$BOT_TOKEN/$method";
+//Salva l'update mandato da Telegram in un JSON e in una variabile
+$update = file_get_contents('php://input');
 
-  if(!$curl = curl_init()){
-    exit;
-  }
-  curl_setopt($curld, CURLOPT_POST, true);
-  curl_setopt($curld, CURLOPT_POSTFIELDS, $data);
-  curl_setopt($curld, CURLOPT_URL, $url);
-  curl_setopt($curld, CURLOPT_RETURNTRANSFER, true);
-  $output = curl_exec($curld);
-  curl_close($curld);
-  return $output;
+//Codifica IL JSON in Array
+$update = json_decode($update, TRUE);
+
+//Prendiamo ID con 'from' quindi da user, chat se è una chat
+$chatID = $update['message']['from']['id'];
+
+//Prendiamo il testo con 'from' quindi da user, 'chat' se è una chat
+$text = $update['message']['text'];
+
+// $agg = json_encode($update,JSON_PRETTY_PRINT);
+
+
+switch($text)
+{
+  case '/start':
+    sendMessage($chatID, "Benvenuto in questo Bot");
+    break;
+  default:
+    sendMessage($chatID, "Ciao, come va?");
+    break;
 }
 
-// $botToken = '1787672753:AAHgbD1VYtlO49DpAPQIi2mgoO6hjLnhabs';
-// $website = 'https://api.telegram.org/bot'.$botToken;
-//
-// //Salva l'update mandato da Telegram in un JSON e in una variabile
-// $update = file_get_contents('php://input');
-//
-// //Codifica IL JSON in Array
-// $update = json_decode($update, TRUE);
-//
-// //Prendiamo ID con 'from' quindi da user, chat se è una chat
-// $chatID = $update['message']['from']['id'];
-//
-// //Prendiamo il testo con 'from' quindi da user, 'chat' se è una chat
-// $text = $update['message']['text'];
-//
-// // $agg = json_encode($update,JSON_PRETTY_PRINT);
-//
-//
-// switch($text)
-// {
-//   case '/start':
-//     sendMessage($chatID, "Benvenuto in questo Bot");
-//     break;
-//   default:
-//     sendMessage($chatID, "Ciao, come va?");
-//     break;
-// }
-//
-// function sendMessage($chatID,$text)
-// {
-//   $url = $GLOBALS[website]."/sendMessage?chat_id=$chatID&text=".urlencode($text);
-//   file_get_contents($url);
-// }
+function sendMessage($chatID,$text)
+{
+  $url = $GLOBALS[website]."/sendMessage?chat_id=$chatID&text=".urlencode($text);
+  file_get_contents($url);
+}
 
 
 //   $BOT_TOKEN = "1787672753:AAHgbD1VYtlO49DpAPQIi2mgoO6hjLnhabs";
